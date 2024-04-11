992729de84a00b2e1e52c7957d5b11cb93fddcdc
Mikkel Wissing <08mikkelwissing@gmail.com>
2024-04-11 13:01:18 +0200

    temp (#13)
    Fetches and displays charging stations from Database.
    TODO:
    Change charging station class to include things like title
    More detailed and polished frontend

--- CSR/backend/app/__init__.py
+++ CSR/backend/app/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask
+from flask_cors import CORS
 from config import Config
 from .extensions import db, login_manager
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
+    CORS(app, resources={r"/*": {"origins": "*"}})
 
 
     db.init_app(app)
